f2f2852cc756501611266b5f1934cef5ec606b4f
kusn <serjikf@yandex.ru>
2021-01-31 17:08:59 +0300

    Отобразил корабль. Выполнил ДЗ_2

---
+++ Home_work2/MyGame_Forms/Asteroids/Ship.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Asteroids
+{
+    class Ship : BaseObject
+    {
+        static Image Image { get; } = Image.FromFile("Images\\ship.png");
+
+        public Ship(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(Image, Pos);
+        }
+
+        public override void Update()
+        {
+            Pos = new Point(Pos.X + Dir.X, Pos.Y);
+            if (Pos.X > Game.Width)
+                Pos = new Point(0, Game.Random.Next(0, Game.Height));
+        }
+
+        /*public int GetX
+        {
+            get
+            {
+                return Pos.X;
+            }
+        }*/
+    }
+}
